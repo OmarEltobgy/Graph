32c729f9877113a1822e22de0891b5dbf6eef7f7
Omar Khaled Eltobgy <omareltobgy@hotmail.com>
2013-08-14 15:07:44 +0200

    Create UVA_341_NonStopTravel.cpp

---
+++ UVA_341_NonStopTravel.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h> 
+#include <vector> 
+#include <queue>
+#include <math.h>
+#include <functional>
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+vector<vii> graph;
+vi distances;
+vi parent;
+priority_queue<ii> pq;
+int source, destination, INF = 10000000;
+
+void SSSP()
+{
+	ii p;
+	int i, childN, extraDelay, parentN, parentMinDelay;
+	pq.push(ii(0, source));
+	distances[source] = 0;
+	while(!pq.empty())
+	{
+		p = pq.top();
+		parentN = p.second;
+		parentMinDelay = p.first;
+		pq.pop();
+		if(distances[parentN] == parentMinDelay)
+		{
+			for(i=0; i<graph[parentN].size(); i++)
+			{
+				childN = graph[parentN][i].second;
+				extraDelay = graph[parentN][i].first;
+				if(parentMinDelay + extraDelay < distances[childN])
+				{
+					distances[childN] = parentMinDelay + extraDelay;
+					parent[childN] = parentN;
+					pq.push(ii(distances[childN], childN));
+				}
+			}
+		}
+	}
+}
+void printPath(int nowNode)
+{
+	if(nowNode == source)
+	{
+		printf(" %d", nowNode);
+		return;
+	}
+	printPath(parent[nowNode]);
+	printf(" %d", nowNode);
+}
+int main()
+{
+	int NI, i, roadsN, j, toWhere, delay, ttt=0;
+	scanf("%d",&NI);
+	while(NI != 0)
+	{
+		ttt ++;
+		graph.resize(NI+1);
+		distances.resize(NI+1);
+		parent.resize(NI+1);
+		for(i=1; i<=NI; i++)
+		{
+			distances[i] = INF;
+			scanf("%d",&roadsN);
+			graph[i].resize(roadsN);
+			for(j=0; j<roadsN; j++)
+			{
+				scanf("%d %d",&toWhere,&delay);
+				graph[i][j] = ii(delay, toWhere);
+			}
+		}
+		scanf("%d %d",&source,&destination);
+		SSSP();
+		printf("Case %d: Path =", ttt);
+		printPath(destination);
+		printf("; %d second delay\n", distances[destination]);
+		for(i=0; i<NI; i++)
+		{
+			graph[i].clear();
+		}
+		graph.clear();
+		scanf("%d",&NI);	
+	}
+	return 0;
+}
